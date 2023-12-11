6f26fcabafe33d88b83492c3595c0bffa99e6fcb
rimao416 <omarkayumba12345@gmail.com>
2023-12-11 16:37:08 +0100

    04_Maps

---
+++ app/(modal)/location-search.tsx
@@ -0,0 +1,95 @@
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import MapView from "react-native-maps";
+import Colors from "../../constants/Colors";
+import { useNavigation } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+
+// {process.env.EXPO_PUBLIC_GOOGLE_API_KEY}
+export default function LocationSearch() {
+  const navigation = useNavigation();
+  const [location, setLocation] = useState({
+    latitude: 51.5078788,
+    longitude: -0.127717,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+  return (
+    <View style={styles.map}>
+      <GooglePlacesAutocomplete
+        placeholder="Search or move the map"
+        fetchDetails={true}
+        onPress={(data, details) => {
+          const point = details?.geometry?.location;
+          if (!point) return;
+          setLocation({
+            ...location,
+            latitude: point.lat,
+            longitude: point.lng,
+          });
+        }}
+        query={{
+          key: "AIzaSyCgeFhxBn3NSReHF67TH7Gz6IfEmwn_bBs",
+          language: "en",
+        }}
+        renderLeftButton={() => (
+          <View style={styles.boxIcon}>
+            <Ionicons name="search-outline" size={24} color={Colors.medium} />
+          </View>
+        )}
+        styles={{
+          container: {
+            flex: 0,
+          },
+          textInput: {
+            backgroundColor: Colors.grey,
+            paddingLeft: 35,
+            borderRadius: 10,
+          },
+          textInputContainer: {
+            padding: 8,
+            backgroundColor: "#fff",
+          },
+        }}
+      />
+      <MapView showsUserLocation={true} style={styles.map} region={location} />
+      <View style={styles.absoluteBox}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.goBack()}
+        >
+          <Text style={styles.buttonText}>Confirm</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+  absoluteBox: {
+    position: "absolute",
+    bottom: 20,
+    width: "100%",
+  },
+  button: {
+    backgroundColor: Colors.primary,
+    padding: 16,
+    margin: 16,
+    alignItems: "center",
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  boxIcon: {
+    position: "absolute",
+    left: 15,
+    top: 18,
+    zIndex: 1,
+  },
+});
